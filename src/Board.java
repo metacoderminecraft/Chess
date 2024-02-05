8b577ae620b0afd79138f2455db935ee7f2a581b
ethan jones <jonese>
2024-02-05 17:53:51 -0500

    I love hasmaps

--- src/Board.java
+++ src/Board.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.HashMap;
 
 import piece.Bishop;
 import piece.King;
@@ -13,6 +14,8 @@ import piece.Side;
 public class Board {
 
     private Piece[][] board = new Piece[8][8];
+    private HashMap<String, Integer> notationConverter;
+    private HashMap<Integer, String> backendConverter;
 
     public void print(){
         for (Piece[] i : board) {
@@ -21,6 +24,7 @@ public class Board {
     }
 
     public Board(){
+        //initializing board
         board[0] = Board.faceRow(Side.BLACK, 0);
         board[1] = Board.pawnRow(Side.BLACK, 1);
         for (int i = 2; i < 6; i++) {
@@ -28,6 +32,26 @@ public class Board {
         }
         board[6] = Board.pawnRow(Side.WHITE, 6);
         board[7] = Board.faceRow(Side.WHITE, 7);
+
+        //setting up hashmaps
+        notationConverter.put("a", 0);
+        notationConverter.put("b", 1);
+        notationConverter.put("c", 2);
+        notationConverter.put("d", 3);
+        notationConverter.put("e", 4);
+        notationConverter.put("f", 5);
+        notationConverter.put("g", 6);
+        notationConverter.put("h", 7);
+
+        backendConverter.put(0, "a");
+        backendConverter.put(1, "b");
+        backendConverter.put(2, "c");
+        backendConverter.put(3, "d");
+        backendConverter.put(4, "e");
+        backendConverter.put(5, "f");
+        backendConverter.put(6, "g");
+        backendConverter.put(7, "h");
+        
     }
 
     private static Piece[] pawnRow(Side side, int y) {
