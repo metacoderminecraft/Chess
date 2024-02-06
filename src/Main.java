1b0039f8320467e6a741c25daa95b4d6592584f2
ethan jones <jonese>
2024-02-06 15:59:21 -0500

    sutff

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,20 @@
 import java.util.Scanner;
 
+import piece.Side;
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         Board board = new Board();
         board.print();
+
+        Side currentSide = Side.WHITE;
+        System.out.println("Make your move");
+        String answer = scanner.nextLine();
+        
+        while (answer != "quit") {
+
+        }
     }
 }
